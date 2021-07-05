c03a0877b79a20fb40047f731f9450f568339185
Michel Krecke <mikreck@hotmail.com>
2021-07-05 12:53:33 +0200

    added json server backend. added CRUD methods of REST. finished StreamList and StreamCreate

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
@@ -8,7 +9,7 @@ import reducers from './reducers';
 
 const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose();
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
